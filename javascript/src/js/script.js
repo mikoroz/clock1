8baa8decf9ec3bc629f31df9d481e2c3c88f7afb
mikoroz <159304348+mikoroz@users.noreply.github.com>
2024-04-23 18:41:35 +0500

    Add files via upload

---
+++ javascript/src/js/script.js
@@ -0,0 +1,38 @@
+function clockTimer()
+{
+  var date = new Date();
+  
+  var time = [date.getHours(),date.getMinutes(),date.getSeconds()]; // |[0] = Hours| |[1] = Minutes| |[2] = Seconds|
+  var dayOfWeek = ["Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"]
+  var days = date.getDay();
+  
+  if(time[0] < 10){time[0] = "0"+ time[0];}
+  if(time[1] < 10){time[1] = "0"+ time[1];}
+  if(time[2] < 10){time[2] = "0"+ time[2];}
+  
+  var current_time = [time[0],time[1],time[2]].join(':');
+  var clock = document.getElementById("clock");
+  var day = document.getElementById("dayOfWeek");
+  
+  clock.innerHTML = current_time;
+  day.innerHTML = dayOfWeek[days];
+  
+  
+  
+  setTimeout("clockTimer()", 1000);
+}
+function showMessage() {
+var message = document.getElementById("message");
+if (message) {
+message.innerText = "privet)";
+message.style.display = "block";
+
+setTimeout(function() {
+message.style.display = "none";
+}, 4000);
+} else {
+console.error("Элемент id = message - не найден");
+}
+}
+
+setInterval(showMessage, 10000);
